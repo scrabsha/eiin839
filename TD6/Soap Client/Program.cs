e585a47598da1da9a71d81273747e597fdb3b87f
Sasha Pourcelot <sasha.pourcelot@etu.univ-cotedazur.fr>
2022-03-29 15:49:50 +0200

    Add more SOAP calls in the client

--- TD6/Soap Client/Program.cs
+++ TD6/Soap Client/Program.cs
@@ -8,7 +8,12 @@ namespace Soap_Client
         {
             var client = new MathOperations.MathsOperationsClient();
             Console.WriteLine(client.Add(41, 1));
+            Console.WriteLine(client.Sub(43, 1));
+            Console.WriteLine(client.Mul(21, 2));
+            Console.WriteLine(client.Div(84, 2));
 
+            // We don't want the terminal to close instantly, so we wait for
+            // the `return` key to be pressed before stopping the program.
             Console.ReadLine();
         }
     }
